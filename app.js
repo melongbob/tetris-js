0545bd32bde0acda59dc63306dbdd1cd07c6be82
Yun <melongbob@gmail.com>
2020-05-25 15:34:35 -0700

    add cells to the grid

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const rowNum = 20
+  const colNum = 10
+  const cellNum = rowNum * colNum
 
+  let htmlElements = ''
+  for (let i = 0; i < cellNum; i++) {
+    htmlElements += '<div></div>'
+  }
+  document.getElementsByClassName('grid')[0].innerHTML = htmlElements
 })
