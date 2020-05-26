267d94f447c69bb7e3e5a7958c07cc61bc82bdfb
Yun <melongbob@gmail.com>
2020-05-25 17:27:26 -0700

    define tetrominoes and draw them on the grid

--- app.js
+++ app.js
@@ -7,5 +7,66 @@ document.addEventListener('DOMContentLoaded', () => {
   for (let i = 0; i < cellNum; i++) {
     htmlElements += '<div></div>'
   }
-  document.getElementsByClassName('grid')[0].innerHTML = htmlElements
+
+  const grid = document.querySelector('.grid')
+  grid.innerHTML = htmlElements
+
+  let cells = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.getElementById('score')
+  const startButton = document.getElementById('start-button')
+
+  const lTetromino = [
+    [1, colNum + 1, colNum * 2 + 1, 2],
+    [colNum, colNum + 1, colNum + 2, colNum * 2 + 2],
+    [1, colNum + 1, colNum * 2 + 1, colNum * 2],
+    [colNum, colNum * 2, colNum * 2 + 1, colNum * 2 + 2]
+  ]
+
+  const oTetromino = [
+    [0, 1, colNum, colNum + 1],
+    [0, 1, colNum, colNum + 1],
+    [0, 1, colNum, colNum + 1],
+    [0, 1, colNum, colNum + 1]
+  ]
+
+  const tTetromino = [
+    [1, colNum, colNum + 1, colNum + 2],
+    [1, colNum + 1, colNum * 2 + 1, colNum + 2],
+    [colNum + 1, colNum + 2, colNum + 3, colNum * 2 + 1],
+    [colNum + 1, 2, colNum + 2, colNum * 2 + 2]
+  ]
+
+  const iTetromino = [
+    [1, colNum + 1, colNum * 2 + 1, colNum * 3 + 1],
+    [colNum, colNum + 1, colNum + 2, colNum + 3],
+    [1, colNum + 1, colNum * 2 + 1, colNum * 3 + 1],
+    [colNum, colNum + 1, colNum + 2, colNum + 3]
+  ]
+
+  const zTetromino = [
+    [1, 2, colNum, colNum + 1],
+    [0, colNum, colNum + 1, colNum * 2 + 1],
+    [1, 2, colNum, colNum + 1],
+    [0, colNum, colNum + 1, colNum * 2 + 1]
+  ]
+
+  const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let currentRotation = 0
+  let random = Math.floor(Math.random()*tetrominoes.length)
+  let current = tetrominoes[random][currentRotation]
+
+  // draw tetromino
+  function draw () {
+    current.forEach(index => {
+      cells[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+  function undraw () {
+    current.forEach(index => {
+      cells[currentPosition + index].classList.remove('tetromino')
+    })
+  }
 })
